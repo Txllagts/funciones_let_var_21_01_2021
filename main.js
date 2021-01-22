f13bc585f3b862cdeb534145e66ad7f1a9cc181d
Jasbeith Andrea Prada Hernández <jasbeith2505@gmail.com>
2021-01-21 21:06:49 -0500

    funciones_let_var_21_01_2021

---
+++ main.js
@@ -0,0 +1,14 @@
+function saludar (p1){
+    console.log(p1);
+}
+function despedir(){
+    console.log("Gracias por ver");
+}
+function despida_usuario(){
+    console.log("Vuelva pronto");
+}
+let dato = prompt("escriba su nombre");
+
+saludar(dato);
+despedir();
+despida_usuario();
